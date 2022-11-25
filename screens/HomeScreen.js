797e7baa346a9bb090b3233f4d333b1b9add2b8d
A-Cobble <alex.cobble42@gmail.com>
2022-11-24 23:44:40 -0800

    added RestaurantCard.js - which is the different carousel lines, added FeaturedRow.js - which includes all the information that is on the RestaurantCard carousel, Added CategoryCard.js - which has the white background for information that is at the bottom of each card

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline";
 import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
 
 
 const HomeScreen = () => {
@@ -20,9 +21,7 @@ const HomeScreen = () => {
         {/* { Header } */}
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image 
-            source={{
-              uri: 'https://links.papareact.com/wru'
-            }}
+            source={{uri: 'https://links.papareact.com/wru'}}
             className='h-7 w-7 bg-gray-300 p-4 rounded-full'
           />
           <View className="flex-1">
@@ -53,7 +52,28 @@ const HomeScreen = () => {
         <ScrollView className="bg-gray-100" contentContainerStyle={{ paddingBottom: 100,}}>
           {/* Categories */}
           <Categories />
+
           {/* Featured Rows */}
+          <FeaturedRow
+            id="123"
+            title="Featured"
+            description="Paid placements from out partners"
+            // featuredCategory="featured"
+          />
+
+          <FeaturedRow
+            id="1234"
+            title="Tasty Discounts"
+            description="Everyone's been enjoying these juicy discounts!"
+            // featuredCategory="discounts"
+          />
+
+          <FeaturedRow
+            id="12345"
+            title="Offers near you!"
+            description="Why not support your local restaurant tonight!"
+            // featuredCategory="offers"
+          />
         </ScrollView>
     </SafeAreaView>
   )
